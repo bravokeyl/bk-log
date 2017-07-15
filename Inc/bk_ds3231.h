b663420c0a6bbf5d5cc3e0e9fc5c47bddb5a323e
bravokeyl <bravokeyl@gmail.com>
2017-07-15 14:24:08 +0530

    check in

---
+++ Inc/bk_ds3231.h
@@ -0,0 +1,16 @@
+/*
+ * bk_ds3231.h
+ *
+ *  Created on: 15-Jul-2017
+ *      Author: xenial
+ */
+
+#ifndef BK_DS3231_H_
+#define BK_DS3231_H_
+
+/* Init */
+void BK_DS3231_Init(void);
+void BK_DS3231_SetDate(void);
+void BK_DS3231_GetDate(void);
+
+#endif /* BK_DS3231_H_ */
