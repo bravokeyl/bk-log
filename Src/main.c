a5e7cc356c5dd4917e8b40d083e55b973eaab3f0
bravokeyl <bravokeyl@gmail.com>
2017-07-17 12:05:45 +0530

    Change baud and sampling of usart 6

--- Src/main.c
+++ Src/main.c
@@ -341,13 +341,13 @@ static void MX_USART6_UART_Init(void)
 {
 
   huart6.Instance = USART6;
-  huart6.Init.BaudRate = 115200;
+  huart6.Init.BaudRate = 9600;
   huart6.Init.WordLength = UART_WORDLENGTH_8B;
   huart6.Init.StopBits = UART_STOPBITS_1;
   huart6.Init.Parity = UART_PARITY_NONE;
   huart6.Init.Mode = UART_MODE_TX_RX;
   huart6.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart6.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart6.Init.OverSampling = UART_OVERSAMPLING_8;
   if (HAL_UART_Init(&huart6) != HAL_OK)
   {
     _Error_Handler(__FILE__, __LINE__);
