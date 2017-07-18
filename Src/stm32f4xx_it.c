23d82228ffd2725877d6c6d1505b878744b6d7c8
bravokeyl <bravokeyl@gmail.com>
2017-07-18 17:59:31 +0530

    Fix build : timer redefinition

--- Src/stm32f4xx_it.c
+++ Src/stm32f4xx_it.c
@@ -156,15 +156,6 @@ void I2C1_EV_IRQHandler(void)
 /**
 * @brief This function handles TIM2 global interrupt.
 */
-void TIM2_IRQHandler(void)
-{
-  /* USER CODE BEGIN TIM2_IRQn 0 */
 
-  /* USER CODE END TIM2_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim2);
-  /* USER CODE BEGIN TIM2_IRQn 1 */
-
-  /* USER CODE END TIM2_IRQn 1 */
-}
 /* USER CODE END 1 */
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
